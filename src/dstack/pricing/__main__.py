b5b0d9c2d59ffd2991ef338ebd8ab8650bdec0dd
Egor Sklyarov <egor.sklyarov.ru@gmail.com>
2023-08-29 13:10:52 +0400

    LambdaLabs pricing catalog

--- src/dstack/pricing/__main__.py
+++ src/dstack/pricing/__main__.py
@@ -7,11 +7,12 @@ import dstack.pricing.storage as storage
 from dstack.pricing.providers.aws import AWSProvider
 from dstack.pricing.providers.azure import AzureProvider
 from dstack.pricing.providers.gcp import GCPProvider
+from dstack.pricing.providers.lambdalabs import LambdaLabsProvider
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("provider", choices=["aws", "azure", "gcp"])
+    parser.add_argument("provider", choices=["aws", "azure", "gcp", "lambdalabs"])
     parser.add_argument("--output", required=True)
     parser.add_argument("--no-filter", action="store_true")
     args = parser.parse_args()
@@ -23,6 +24,8 @@ def main():
         provider = AzureProvider(os.getenv("AZURE_SUBSCRIPTION_ID"))
     elif args.provider == "gcp":
         provider = GCPProvider(os.getenv("GCP_PROJECT_ID"))
+    elif args.provider == "lambdalabs":
+        provider = LambdaLabsProvider()
     else:
         exit(f"Unknown provider {args.provider}")
 
---
+++ src/dstack/pricing/providers/lambdalabs.py
@@ -0,0 +1,69 @@
+import re
+from typing import Tuple
+
+import bs4
+import requests
+
+from dstack.pricing.models import InstanceOffer
+from dstack.pricing.providers import AbstractProvider
+
+specs_page_url = "https://lambdalabs.com/service/gpu-cloud"
+
+
+class LambdaLabsProvider(AbstractProvider):
+    def get(self) -> list[InstanceOffer]:
+        soup = bs4.BeautifulSoup(requests.get(specs_page_url).text, "html.parser")
+        table = soup.find("span", text="GPUs").find_parent("table")
+
+        offers = []
+        for row in table.find("tbody").find_all("tr"):
+            cells = row.find_all("td")
+            gpu_memory = parse_memory(cells[1].text)
+            instance_name, gpu_count, gpu_name = parse_name(cells[0].text, gpu_memory)
+            offer = InstanceOffer(
+                instance_name=instance_name,
+                price=parse_price(cells[5].text),
+                cpu=int(cells[2].text),
+                memory=parse_memory(cells[3].text),
+                gpu_count=gpu_count,
+                gpu_name=gpu_name,
+                gpu_memory=gpu_memory,
+                spot=False,
+            )
+            offers.append(offer)
+            if offer.gpu_name == "A100" and offer.gpu_count == 1:
+                offer = offer.model_copy()
+                offer.instance_name += "_sxm4"
+                offers.append(offer)
+        return offers
+
+
+def parse_memory(v: str) -> float:
+    r = re.match(r"^([\d.]+) ?([GT])i?B$", v)
+    value, unit = r.groups()
+    value = float(value)
+    if unit == "T":
+        value *= 1000
+    return value
+
+
+def parse_price(v: str) -> float:
+    r = re.match(r"^\$([\d.]+) / hr$", v)
+    return float(r.group(1))
+
+
+def parse_name(v: str, gpu_memory: float) -> Tuple[str, int, str]:
+    """Returns instance name, number of GPUs, and GPU name"""
+    v = v.replace("RTX 6000", "RTX6000")
+    r = re.match(r"^(\d)x NVIDIA(?: RTX| Quadro| Tesla)? ([A-Z]+\d+)", v)
+    count, gpu_name = r.groups()
+    count = int(count)
+
+    suffix = ""
+    if gpu_name == "H100" and count == 1:
+        suffix = "_pcie"
+    if gpu_name == "H100" and count == 8:
+        suffix = "_sxm5"
+    if gpu_name == "A100" and count == 8 and int(gpu_memory) == 80:
+        suffix = "_80gb_sxm4"
+    return f"gpu_{count}x_{gpu_name.lower()}{suffix}", count, gpu_name
