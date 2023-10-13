258a6611f26fc2cff9243c252b56a5bd432df932
Egor Sklyarov <egor.sklyarov.ru@gmail.com>
2023-10-13 19:10:22 +0400

    Import provider if needed

--- src/gpuhunt/__main__.py
+++ src/gpuhunt/__main__.py
@@ -4,12 +4,6 @@ import os
 import sys
 
 import gpuhunt._storage as storage
-from gpuhunt.providers.aws import AWSProvider
-from gpuhunt.providers.azure import AzureProvider
-from gpuhunt.providers.gcp import GCPProvider
-from gpuhunt.providers.lambdalabs import LambdaLabsProvider
-from gpuhunt.providers.tensordock import TensorDockProvider
-from gpuhunt.providers.vastai import VastAIProvider
 
 
 def main():
@@ -28,16 +22,22 @@ def main():
     )
 
     if args.provider == "aws":
+        from gpuhunt.providers.aws import AWSProvider
         provider = AWSProvider(os.getenv("AWS_CACHE_PATH"))
     elif args.provider == "azure":
+        from gpuhunt.providers.azure import AzureProvider
         provider = AzureProvider(os.getenv("AZURE_SUBSCRIPTION_ID"))
     elif args.provider == "gcp":
+        from gpuhunt.providers.gcp import GCPProvider
         provider = GCPProvider(os.getenv("GCP_PROJECT_ID"))
     elif args.provider == "lambdalabs":
+        from gpuhunt.providers.lambdalabs import LambdaLabsProvider
         provider = LambdaLabsProvider(os.getenv("LAMBDALABS_TOKEN"))
     elif args.provider == "tensordock":
+        from gpuhunt.providers.tensordock import TensorDockProvider
         provider = TensorDockProvider()
     elif args.provider == "vastai":
+        from gpuhunt.providers.vastai import VastAIProvider
         provider = VastAIProvider()
     else:
         exit(f"Unknown provider {args.provider}")
