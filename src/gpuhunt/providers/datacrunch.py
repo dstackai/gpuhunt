40f62ebc05ba0b8be33a798fe2ce11cf39779af4
Sergey Mezentsev <sergeyme@dstack.ai>
2023-11-14 12:22:26 +0300

    Add gpu map

--- src/gpuhunt/providers/datacrunch.py
+++ src/gpuhunt/providers/datacrunch.py
@@ -21,7 +21,7 @@ class DataCrunchProvider(AbstractProvider):
                 cpu=instance.cpu["number_of_cores"],
                 memory=instance.memory["size_in_gigabytes"],
                 gpu_count=instance.gpu["number_of_gpus"],
-                gpu_name=instance.gpu["description"],
+                gpu_name=gpu_name(instance.gpu["description"]),
                 gpu_memory=instance.gpu_memory["size_in_gigabytes"],
                 spot=False,
             )
@@ -29,3 +29,34 @@ class DataCrunchProvider(AbstractProvider):
         ]
 
         return items
+
+
+def gpu_name(name: str) -> str:
+    gpu_map = {
+        "1x H100 SXM5 80GB": "H100",
+        "2x H100 SXM5 80GB": "H100",
+        "4x H100 SXM5 80GB": "H100",
+        "4x H100 SXM5 80GB": "H100",
+        "8x H100 SXM5 80GB": "H100",
+        "1x A100 SXM4 80GB": "A100",
+        "2x A100 SXM4 80GB": "A100",
+        "4x A100 SXM4 80GB": "A100",
+        "8x A100 SXM4 80GB": "A100",
+        "1x A100 SXM4 40GB": "A100",
+        "2x A100 SXM4 40GB": "A100",
+        "4x A100 SXM4 40GB": "A100",
+        "8x A100 SXM4 40GB": "A100",
+        "1x NVidia RTX6000 Ada 48GB": "RTX6000",
+        "2x NVidia RTX6000 Ada 48GB": "RTX6000",
+        "4x NVidia RTX6000 Ada 48GB": "RTX6000",
+        "8x NVidia RTX6000 Ada 48GB": "RTX6000",
+        "1x NVidia RTX A6000 48GB": "A6000",
+        "2x NVidia RTX A6000 48GB": "A6000",
+        "4x NVidia RTX A6000 48GB": "A6000",
+        "8x NVidia RTX A6000 48GB": "A6000",
+        "1x NVidia Tesla V100 16GB": "V100",
+        "2x NVidia Tesla V100 16GB": "V100",
+        "4x NVidia Tesla V100 16GB": "V100",
+        "8x NVidia Tesla V100 16GB": "V100",
+    }
+    return gpu_map.get(name)
