32823cbb567deec761a8818badcebd63544b3ca5
peterschmidt85 <andrey.cheptsov@gmail.com>
2023-12-18 16:44:01 +0100

    Revert "- Support disk size #30 (bugfix 2)"
    This reverts commit 9ae0e97a1311d827abc0af4ebf815f3f50734b76.

--- src/gpuhunt/_internal/models.py
+++ src/gpuhunt/_internal/models.py
@@ -73,10 +73,10 @@ class CatalogItem(RawCatalogItem):
 
     @staticmethod
     def from_dict(v: dict, *, provider: Optional[str] = None) -> "CatalogItem":
-        raw = RawCatalogItem.from_dict(v)
+        raw = asdict(RawCatalogItem.from_dict(v))
         if not raw.disk_size:
             raw.disk_size = 100.0  # the default value for backward compatibility
-        return CatalogItem(provider=provider, **asdict(raw))
+        return CatalogItem(provider=provider, **raw)
 
 
 @dataclass
