e87aac64daafbc2682d2a25d8bfbd9bb4e3a59fd
Egor Sklyarov <egor.sklyarov.ru@gmail.com>
2024-01-12 12:06:48 +0100

    Refactor Azure integrity tests

--- src/integrity_tests/test_azure.py
+++ src/integrity_tests/test_azure.py
@@ -1,16 +1,19 @@
+import csv
 from pathlib import Path
+from typing import List
 
 import pytest
 
 
 @pytest.fixture
-def data(catalog_dir: Path) -> str:
-    return (catalog_dir / "azure.csv").read_text()
+def data_rows(catalog_dir: Path) -> List[dict]:
+    with open(catalog_dir / "azure.csv", "r") as f:
+        return list(csv.DictReader(f))
 
 
 class TestAzureCatalog:
-    def test_standard_d2s_v3_locations(self, data: str):
-        locations = [
+    def test_standard_d2s_v3_locations(self, data_rows: List[dict]):
+        expected_locations = {
             "attatlanta1",
             "attdallas1",
             "attdetroit1",
@@ -70,24 +73,29 @@ class TestAzureCatalog:
             "westus",
             "westus2",
             "westus3",
-        ]
-        assert all(f"\nStandard_D2s_v3,{i}," in data for i in locations)
+        }
+        locations = set(
+            row["location"] for row in data_rows if row["instance_name"] == "Standard_D2s_v3"
+        )
+        assert expected_locations == locations
 
-    def test_spots_presented(self, data: str):
-        assert ",True," in data
+    def test_spots_presented(self, data_rows: List[dict]):
+        assert any(row["spot"] == "True" for row in data_rows)
 
-    def test_ondemand_presented(self, data: str):
-        assert ",False," in data
+    def test_ondemand_presented(self, data_rows: List[dict]):
+        assert any(row["spot"] == "False" for row in data_rows)
 
-    def test_gpu_presented(self, data: str):
-        gpus = [
+    def test_gpu_presented(self, data_rows: List[dict]):
+        expected_gpus = {
             "A100",
             "A10",
             "T4",
             "V100",
-        ]
-        assert all(f",{i}," in data for i in gpus)
+        }
+        gpus = set(row["gpu_name"] for row in data_rows if row["gpu_name"])
+        assert expected_gpus == gpus
 
-    def test_both_a100_presented(self, data: str):
-        assert ",A100,40.0," in data
-        assert ",A100,80.0," in data
+    def test_both_a100_presented(self, data_rows: List[dict]):
+        expected_gpu_memory = {"40.0", "80.0"}
+        gpu_memory = set(row["gpu_memory"] for row in data_rows if row["gpu_name"] == "A100")
+        assert expected_gpu_memory == gpu_memory
