a3e607b82eb01b2d5268f5d5db8e54eaa75720f8
Victor Skvortsov <vds003@gmail.com>
2024-01-30 11:58:47 +0500

    Export KNOWN_GPUS

--- src/gpuhunt/__init__.py
+++ src/gpuhunt/__init__.py
@@ -1,4 +1,4 @@
 from gpuhunt._internal.catalog import Catalog
-from gpuhunt._internal.constraints import matches
+from gpuhunt._internal.constraints import KNOWN_GPUS, GPUInfo, matches
 from gpuhunt._internal.default import default_catalog, query
 from gpuhunt._internal.models import CatalogItem, QueryFilter, RawCatalogItem
