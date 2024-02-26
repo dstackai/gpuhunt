de76f1b9dcb8709a196b5b8a52c9ef8f735555ad
Egor Sklyarov <egor.sklyarov.ru@gmail.com>
2024-02-26 13:05:35 +0100

    Fix cudo integrity test import

--- src/integrity_tests/test_cudo.py
+++ src/integrity_tests/test_cudo.py
@@ -5,7 +5,7 @@ from typing import List
 
 import pytest
 
-from src.gpuhunt.providers.cudo import GPU_MAP
+from gpuhunt.providers.cudo import GPU_MAP
 
 
 @pytest.fixture
@@ -21,16 +21,14 @@ def select_row(rows, name: str) -> List[str]:
 
 
 def test_locations(data_rows):
-    expected = set(
-        (
-            "no-luster-1",
-            "se-smedjebacken-1",
-            "gb-london-1",
-            "se-stockholm-1",
-            "us-newyork-1",
-            "us-santaclara-1",
-        )
-    )
+    expected = {
+        "no-luster-1",
+        "se-smedjebacken-1",
+        "gb-london-1",
+        "se-stockholm-1",
+        "us-newyork-1",
+        "us-santaclara-1",
+    }
     locations = select_row(data_rows, "location")
     assert set(locations) == expected
 
