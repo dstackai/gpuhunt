acedf1c75e63b89a9969d7f0f351d27b5d526b87
Sergey Mezentsev <sergeyme@dstack.ai>
2024-04-08 12:05:38 +0300

    Runpod's integrity test was fixed

--- src/integrity_tests/test_runpod.py
+++ src/integrity_tests/test_runpod.py
@@ -22,12 +22,12 @@ def select_row(rows, name: str) -> List[str]:
 def test_locations(data_rows):
     expected = {
         "CA-MTL-1",
+        "CA-MTL-2",
         "EU-NL-1",
         "EU-RO-1",
         "EU-SE-1",
         "EUR-IS-1",
         "EUR-IS-2",
-        "EUR-NO-1",
         "US-OR-1",
     }
     locations = select_row(data_rows, "location")
