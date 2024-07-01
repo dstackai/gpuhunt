b36f3e195fead97f8d0ff7934ded5c159065f50c
Bihan  Rana <bihan@Bihans-MacBook-Pro.local>
2024-07-01 16:14:13 +0545

    Add TPU check condition in matches function

--- src/gpuhunt/__init__.py
+++ src/gpuhunt/__init__.py
@@ -1,5 +1,4 @@
 from gpuhunt._internal.catalog import Catalog
-from gpuhunt._internal.constraints import KNOWN_GPUS, GPUInfo, matches, tpu_matches
+from gpuhunt._internal.constraints import KNOWN_GPUS, GPUInfo, matches
 from gpuhunt._internal.default import default_catalog, query
 from gpuhunt._internal.models import CatalogItem, QueryFilter, RawCatalogItem
-from gpuhunt._internal.utils import _is_tpu
