dee29c34dd47dc2a74e95835370fca3c4ec4683a
Slawek <deveses80@proton.me>
2025-07-04 00:25:26 -0700

    formatting

--- src/integrity_tests/test_cloudrift.py
+++ src/integrity_tests/test_cloudrift.py
@@ -6,19 +6,24 @@ import pytest
 
 from gpuhunt.providers.cloudrift import GPU_MAP
 
+
 @pytest.fixture
 def data_rows(catalog_dir: Path) -> list[dict]:
     with open(catalog_dir / "cloudrift.csv") as f:
         return list(csv.DictReader(f))
 
+
 def select_row(rows, name: str) -> list[str]:
     return [r[name] for r in rows if r[name]]
+
 
 def test_gpu_present(data_rows: list[dict]):
     expected_gpus = [gpu for _, gpu in GPU_MAP]
     gpus = select_row(data_rows, "gpu_name")
-    gpus = list(dict.fromkeys(gpus))    
-    assert set(gpus).issubset(set(expected_gpus)), f"Found unexpected GPUs: {set(gpus) - set(expected_gpus)}"
+    gpus = list(dict.fromkeys(gpus))
+    assert set(gpus).issubset(
+        set(expected_gpus)
+    ), f"Found unexpected GPUs: {set(gpus) - set(expected_gpus)}"
 
 
 # TODO: Add 3, 4, 5, ... 8
