fca2889bbe21496afdd21008f36e36ad98fcf73b
Dmitry Meyer <me@undef.im>
2026-07-03 09:44:10 +0000

    Expose find_accelerators

--- src/gpuhunt/__init__.py
+++ src/gpuhunt/__init__.py
@@ -1,31 +1,34 @@
-# ruff: noqa: F401
 import warnings
 
-from gpuhunt._internal.catalog import Catalog
+from gpuhunt._internal.catalog import Catalog as Catalog
 from gpuhunt._internal.constraints import (
-    KNOWN_ACCELERATORS,
-    KNOWN_AMD_GPUS,
-    KNOWN_INTEL_ACCELERATORS,
-    KNOWN_NVIDIA_GPUS,
-    KNOWN_TENSTORRENT_ACCELERATORS,
-    KNOWN_TPUS,
-    correct_gpu_memory_gib,
-    is_nvidia_superchip,
-    matches,
+    KNOWN_ACCELERATORS as KNOWN_ACCELERATORS,
+    KNOWN_AMD_GPUS as KNOWN_AMD_GPUS,
+    KNOWN_INTEL_ACCELERATORS as KNOWN_INTEL_ACCELERATORS,
+    KNOWN_NVIDIA_GPUS as KNOWN_NVIDIA_GPUS,
+    KNOWN_TENSTORRENT_ACCELERATORS as KNOWN_TENSTORRENT_ACCELERATORS,
+    KNOWN_TPUS as KNOWN_TPUS,
+    correct_gpu_memory_gib as correct_gpu_memory_gib,
+    find_accelerators as find_accelerators,
+    is_nvidia_superchip as is_nvidia_superchip,
+    matches as matches,
 )
-from gpuhunt._internal.default import default_catalog, query
+from gpuhunt._internal.default import (
+    default_catalog as default_catalog,
+    query as query,
+)
 from gpuhunt._internal.models import (
-    AcceleratorInfo,
-    AcceleratorVendor,
-    AMDGPUInfo,
-    CatalogItem,
-    CPUArchitecture,
-    IntelAcceleratorInfo,
-    NvidiaGPUInfo,
-    QueryFilter,
-    RawCatalogItem,
-    TenstorrentAcceleratorInfo,
-    TPUInfo,
+    AcceleratorInfo as AcceleratorInfo,
+    AcceleratorVendor as AcceleratorVendor,
+    AMDGPUInfo as AMDGPUInfo,
+    CatalogItem as CatalogItem,
+    CPUArchitecture as CPUArchitecture,
+    IntelAcceleratorInfo as IntelAcceleratorInfo,
+    NvidiaGPUInfo as NvidiaGPUInfo,
+    QueryFilter as QueryFilter,
+    RawCatalogItem as RawCatalogItem,
+    TenstorrentAcceleratorInfo as TenstorrentAcceleratorInfo,
+    TPUInfo as TPUInfo,
 )
 
 # Deprecated aliases
